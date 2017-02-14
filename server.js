5d9e38f6a9ff85945ded6a79bd6a7add8917637d
Vinod-C <vinod.chelsea@gmail.com>
2017-02-14 13:34:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-1', function (req, res) {
+  res.send('Article1 has been requested and it will be served');    
+});
+
+app.get('article-2', function (req, res) {
+  res.send('Article2 has been requested and it will be served');    
+});
+
+app.get('article-3', function (req, res) {
+  res.send('Article3 has been requested and it will be served');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
